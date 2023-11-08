6877859a8cbd325249376590a6c579d88845b515
Your Name <you@example.com>
2023-11-08 21:44:23 +0700

    dangNhap_dangKy

---
+++ view/DangNhap_DangKy/login.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: black;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+/* Add padding to containers */
+.container {
+  padding: 16px;
+  background-color: white;
+}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type=text]:focus, input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+/* Overwrite default styles of hr */
+hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+}
+
+/* Set a style for the submit button */
+.registerbtn {
+  background-color: #04AA6D;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+.registerbtn:hover {
+  opacity: 1;
+}
+
+/* Add a blue text color to links */
+a {
+  color: dodgerblue;
+}
+
+/* Set a grey background color and center the text of the "sign in" section */
+.signin {
+  background-color: #f1f1f1;
+  text-align: center;
+}
+span.psw {
+    float: right;
+    padding-top: 16px;
+  }
+
+</style>
+</head>
+<body>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Handle data after the user submits the form
+  // For example:
+  $username = $_POST['username'];
+  $password = $_POST['psw'];
+
+  // Continue processing data here...
+}
+?>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+  <div class="container">
+    <h1>Xin Chào! Vui Lòng Đăng Nhập</h1>
+    <hr>
+
+    <label for="username"><b></b></label>
+    <input type="text" placeholder="Tên Đăng Nhập" name="username" id="username" required>
+
+    <label for="psw"><b></b></label>
+    <input type="password" placeholder="Mật Khẩu" name="psw" id="psw" required>
+    <span class="psw"><a href="#">Quên Mật Khẩu?</a></span>
+    <hr>
+    <button type="submit" class="registerbtn">Đăng Nhập</button>
+  </div>
+  
+  <div class="container signin">
+    <p>Bạn Chưa Có Tài Khoản? <a href="register.php">Đăng Ký</a>.</p>
+  </div>
+</form>
+
+</body>
+</html>
