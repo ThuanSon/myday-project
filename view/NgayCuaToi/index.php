a0fe16a5b1379a682f90b79b0f59b66af2c3e4fb
HaiTrieu <trieuphan3701@gmail.com>
2023-11-08 14:12:55 +0700

    delete file and add view/ngaycuatoi/index.php, style.css not final

---
+++ view/NgayCuaToi/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HaiTrieu</title>
+    <link rel="stylesheet" href="./style.css">
+    <style>
+    .nomal {
+        width: 100%;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <!-- <p>hello</p> -->
+        <div class="header">header</div>
+        <div class="task-menu">menu</div>
+        <div class="content-todo">
+            <div class="header-content-todo">
+                <span>icon</span>
+                <h2>Công việc hằng ngày</h2>
+            </div>
+
+            <div class="todo-all-content">
+                    <div class="todo-all-work">
+                    <div class="todo-work-label">
+                        <b>Việc cần làm</b>
+                    </div>
+
+                    <form class="input-form input-form-work">
+                        <input type="text" id="text-input-todo-1" class="text-input" placeholder="Input 1">
+                        <input type="text" id="text-input-todo-2" class="text-input" placeholder="Input 2">
+                        <input type="text" id="text-input-todo-3" class="text-input" placeholder="Input 3">
+                        <input type="text" id="text-input-todo-4" class="text-input" placeholder="Input 4">
+                        <input type="text" id="text-input-todo-5" class="text-input" placeholder="Input 5">
+                        <input type="text" id="text-input-todo-6" class="text-input" placeholder="Input 6">
+                    </form>
+
+                </div>
+
+                <div class="todo-all-emoji">
+                    <div class="todo-work-label">
+                        <b>Việc cần làm</b>
+                    </div>
+
+                    <form class="input-form input-form-emoji">
+                        <input type="text" id="text-input-todo-1" class="text-input" placeholder="Input 1">
+                        <input type="text" id="text-input-todo-2" class="text-input" placeholder="Input 2">
+                        <input type="text" id="text-input-todo-3" class="text-input" placeholder="Input 3">
+                        <input type="text" id="text-input-todo-4" class="text-input" placeholder="Input 4">
+                        <input type="text" id="text-input-todo-5" class="text-input" placeholder="Input 5">
+                        <input type="text" id="text-input-todo-6" class="text-input" placeholder="Input 6">
+                    </form>
+
+                </div>
+
+                <div class="todo-all-water">
+                    <div class="todo-work-label">
+                        <b>Việc cần làm</b>
+                    </div>
+
+                    <form class="input-form input-form-water">
+                        <input type="text" id="text-input-todo-1" class="text-input" placeholder="Input 1">
+                        <input type="text" id="text-input-todo-2" class="text-input" placeholder="Input 2">
+                        <input type="text" id="text-input-todo-3" class="text-input" placeholder="Input 3">
+                        <input type="text" id="text-input-todo-4" class="text-input" placeholder="Input 4">
+                        <input type="text" id="text-input-todo-5" class="text-input" placeholder="Input 5">
+                        <input type="text" id="text-input-todo-6" class="text-input" placeholder="Input 6">
+                    </form>
+
+                </div>
+            </div>
+            
+            
+            
+        </div>
+    </div>
+
+</body>
+
+</html>
