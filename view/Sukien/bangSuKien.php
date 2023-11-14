7746459f6ef91ae2d8e9f851daeb8f329e3f44c7
acma1169 <anhdung535a@gmail.com>
2023-11-14 22:13:17 +0700

    Bảng sự kiện và Trang chủ

---
+++ view/Sukien/bangSuKien.php
@@ -0,0 +1,62 @@
+<?php
+$servername = "localhost";
+$database = "mydayhandbook";
+$username = "root";
+$password = "";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+$sql = "SELECT TenSK,ThoiGian,UserName FROM sukien ORDER BY ThoiGian ASC";
+
+$result = $conn->query($sql);
+
+$sukien = $result->fetch_all(MYSQLI_ASSOC);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+mysqli_close($conn);
+?>
+
+
+    <div id='calendar'></div>
+    <script src="
+https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/index.global.min.js
+"></script>
+    <script>
+        const sukiens = <?php echo json_encode($sukien) ?>
+
+        const events = sukiens.map(sukien => ({
+            title: sukien.TenSK,
+            start: sukien.ThoiGian,
+        }))
+        
+        console.log(events);
+        document.addEventListener('DOMContentLoaded', function() {
+            const calendarEl = document.getElementById('calendar');
+            const calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                headerToolbar: {
+                    left: 'prev,next today addNewButton', // Add your custom button name here
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                customButtons: {
+                    addNewButton: {
+                        text: 'Add New',
+                        click: function() {
+                            // Handle the click event of the custom button
+                            // You can open a modal, redirect to another page, etc.
+                            window.location.href = "./view/Sukien/themmoi.php";
+                        }
+                    }
+                },
+                // eventContent: {
+                //     domNodes: events
+                // },
+                events: events
+
+            });
+            calendar.render();
+        });
+    </script>
